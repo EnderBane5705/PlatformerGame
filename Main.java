b4f21e3b739152a67a74dcf014e473e81251e825
EnderBane <end3rbane@outlook.com>
2025-09-02 18:17:35 -0700

    update

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import src.Player;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("PlatformerGame running initializing...");
+		System.out.println("PlatformerGame initializing...");
 	}
 }
