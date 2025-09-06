df903ccc4e119ab53fbb30a5a193c57837a8f939
EnderBane <end3rbane@outlook.com>
2025-09-05 17:30:07 -0700

    update

--- Main.java
+++ Main.java
@@ -1,13 +1,30 @@
 import java.awt.*;
 import java.swing.*;
-import java.util.*
+import java.util.*;
 import java.awt.geom.*;
 import java.awt.event.*;
 
 import src.Display.Canvas.java;
 
-public class Main {
+public class Main extends JFrame {
+	
+	public Main() { //initialize window
+		Canvas canvas = new Canvas(); //initialize game screen
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		add(canvas);
+		setSize(1000, 800);
+		setResizable(false);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(true);
+		
+		//add key listener here
+		
+		new Game(); //initialize game loop
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("PlatformerGame initializing...");
+		new Main(); //start game
+		System.out.println("Game running...");
 	}
 }
--- src/Game.java
+++ src/Game.java
@@ -3,6 +3,9 @@ package src;
 import java.util.*;
 
 public class Game {
+	
+	public static ArrayList<Entity> actors = new ArrayList<Entity>(); //other classes can add actors to the list
+	
 	public Game() {
 		
 	}
